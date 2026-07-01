e75ef596e81f78bac50bab6c297b39c00fd80846
andrew <easycarry041@gmail.com>
2026-07-01 11:51:44 +0800

    Thats a good spot

--- resources/views/components/document-images.blade.php
+++ resources/views/components/document-images.blade.php
@@ -57,6 +57,7 @@
                         </form>
                     @endif
                 </div>
+            @endif
         @endforeach
         @if($images->count() > $limit)
             <span class="flex {{ $thumbClass }} items-center justify-center rounded-lg bg-gray-100 text-xs font-semibold text-gray-600">+{{ $images->count() - $limit }}</span>
